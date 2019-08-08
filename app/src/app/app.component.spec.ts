988e221f8ee74cb7d3694e818d0de286e26c3f28
Suraj Patel <Surajpatel527@gmail.com>
2019-08-08 19:16:44 -0400

    Finished app code

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Dating-app'`, () => {
+  it(`should have as title 'Sakura'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Dating-app');
+    expect(app.title).toEqual('Sakura');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Dating-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Sakura!');
   });
 });
